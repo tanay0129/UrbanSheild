cea5819758b65bd764ec6c1b3b2d2b42ac4c516f
tanay0129 <tanaykarthik01@gmail.com>
2025-04-29 10:19:37 +0530

    Add files via upload

---
+++ theme based project/script.js
@@ -0,0 +1,5 @@
+// Minor functionality - Button Click Example
+function checkArea() {
+  alert('Redirecting to Citizen Portal...');
+  // You can replace this with window.location.href = "citizen-portal.html" later
+}
